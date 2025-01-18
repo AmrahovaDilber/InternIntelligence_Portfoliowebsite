797a744e2a232d745a98124235c89a0cfbab010d
AmrahovaDilber <amrahovad911@gmail.com>
2025-01-18 09:21:49 +0400

    changed responsive design

--- src/components/timeline.tsx
+++ src/components/timeline.tsx
@@ -46,7 +46,7 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
   return (
     <div className="w-full font-sans md:px-6" ref={containerRef}>
       <div className="text-center mt-14">
-        <h1 className="text-4xl md:text-6xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 animate-gradient">
+        <h1 className="text-4xl md:text-6xl font-bold text-transparent bg-clip-text bg-gradient-to-r mb-4 from-blue-500 via-purple-500 to-pink-500 animate-gradient">
           Work Experience
         </h1>
       </div>
@@ -55,13 +55,13 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
           <div key={index} className="flex justify-start pt-10 md:pt-30 md:gap-10">
             <div className="sticky flex flex-col md:flex-row z-40 items-center top-16 self-start max-w-xs lg:max-w-sm md:w-full">
               <div className="h-10 absolute left-3 md:left-3 w-10 rounded-full bg-white flex items-center justify-center">
-                <div className="h-4 w-4 rounded-full bg-gradient-to-t from-purple-500 via-blue-500 border border-neutral-300 p-2" />
+                <div className="h-4 w-4 rounded-full bg-gradient-to-t from-purple-500 via-blue-500 border border-neutral-300 p-2 " />
               </div>
               <h3 className="bg-[rgb(23,11,42)] border p-2 rounded-lg hidden md:block text-lg md:text-2xl font-bold text-gray-300 md:pl-20">
                 {item.title}
               </h3>
             </div>
-            <div className="relative pl-16 ml-20 lg:ml-0 sm:pl-12 py-8 w-full backdrop-blur-lg bg-black bg-opacity-40 rounded-lg">
+            <div className="relative pl-8 ml-20 lg:ml-0 sm:pl-12 py-8 w-full backdrop-blur-lg bg-black bg-opacity-40 rounded-lg">
               <h3 className="md:hidden block text-2xl mb-4 text-left font-bold text-neutral-500">
                 {item.title}
               </h3>
